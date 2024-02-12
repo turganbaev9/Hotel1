555c024196027941ce6e9755077bb7fe873c186d
Bektursun_K <beka.turganbaev98@gmail.com>
2024-02-12 18:10:32 +0300

    created repositoryies

---
+++ src/main/java/kg/mega/hotel1/crud/ClientRepo.java
@@ -0,0 +1,9 @@
+package kg.mega.hotel1.crud;
+
+import kg.mega.hotel1.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepo extends JpaRepository<Client,Long> {
+}
